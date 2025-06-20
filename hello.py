a9b41da3b151170db505e4e3dc94913748c92b42
Carlos Javier De la Cruz Lopez/ACR Enhancement /SEM-P/Engineer/Samsung Electronics <c3.lopez@samsung.com>
2025-06-20 08:14:31 -0700

    refactor: using hashmaps instead 2-D lists

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/user')
 def hello_world():
-    return "<p>Hello User Refactores</p>"
+    return "<p>Hello User Refactor</p>"
 
 @app.route('/edit')
 def edit():
